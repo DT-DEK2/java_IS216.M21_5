ba2f76988d9a4c6ce92ab90d8ae85a13ab509482
DT47UIT <20520434@gm.uit.edu.vn>
2022-05-17 09:45:18 +0700

    Update

---
+++ QuanLiF0/src/com/da/qlf0/service/BenhNhanService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.da.qlf0.service;
+
+import com.da.qlf0.model.BenhNhan;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Minh Cuong
+ */
+public interface BenhNhanService {
+    public ArrayList<BenhNhan> getList();
+
+  
+}
